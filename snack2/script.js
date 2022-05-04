c6b2bc4301ff49230c1d5b6da29bbebd4ef1699a
Gabry17 <gbianchi528@gmail.com>
2022-05-04 17:47:06 +0200

    completato snack2

--- snack2/script.js
+++ snack2/script.js
@@ -4,39 +4,10 @@ const array = [];
 
 let sum = 0;
 
-while(sum < 50){ 
-    for (let i = 0; i < array.length; i++) {
-        const thisElement = array[i];
-        sum = sum + thisElement;
-        console.log(sum); 
-    }
+while (sum < 50) {
     let userN = parseInt(prompt('inserisci un numero'));
+    sum += userN;
     array.push(userN);
-    console.log(array,sum);
 }
 
-
-
-// let sum = 0;
-
-// do{
-//     let userN = parseInt(prompt('inserisci un numero'));
-//     array.push(userN);
-//     for (let i = 0; i < array.length; i++) {
-//         const thisElement = array[i];
-//         sum += thisElement;
-//     } 
-
-// } while(sum < 50);
-
-
-
-// console.log(array,sum);
-
-// while(sum < 50){
-//     const userN = parseInt(prompt('inserisci un numero')); 
-//     array.push(userN);
-    
-    
-// }
-// console.log(array,sum);
+console.log(array,sum);
