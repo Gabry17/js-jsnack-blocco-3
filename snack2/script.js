f8abd72fb27ab85fa6bc54722a54362f8eeea3c2
Gabry17 <gbianchi528@gmail.com>
2022-05-04 16:08:44 +0200

    snack2

---
+++ snack2/script.js
@@ -0,0 +1,27 @@
+//crea array vuoto
+//chuedi all utente di inserire un number e pushalo nell array finche la sommma diventa <= a 50
+const array = [];
+
+let sum = 0;
+
+do{
+    let userN = parseInt(prompt('inserisci un numero'));
+    array.push(userN);
+    for (let i = 0; i < array.length; i++) {
+        const thisElement = array[i];
+        sum += thisElement;
+    } 
+
+} while(sum < 50);
+
+
+
+console.log(array,sum);
+
+// while(sum < 50){
+//     const userN = parseInt(prompt('inserisci un numero')); 
+//     array.push(userN);
+    
+    
+// }
+// console.log(array,sum);
