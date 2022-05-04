198f04ebab0f8b2b08d4df529d6ab457ef3ed07f
Gabry17 <gbianchi528@gmail.com>
2022-05-04 17:07:51 +0200

    cicilo while che mi calcola la somma mi chiede di inserire numeri e pusharlo nell array

--- snack2/script.js
+++ snack2/script.js
@@ -4,19 +4,34 @@ const array = [];
 
 let sum = 0;
 
-do{
-    let userN = parseInt(prompt('inserisci un numero'));
-    array.push(userN);
+while(sum < 50){ 
     for (let i = 0; i < array.length; i++) {
         const thisElement = array[i];
-        sum += thisElement;
-    } 
+        sum = sum + thisElement;
+        console.log(sum); 
+    }
+    let userN = parseInt(prompt('inserisci un numero'));
+    array.push(userN);
+    console.log(array,sum);
+}
 
-} while(sum < 50);
+
+
+// let sum = 0;
+
+// do{
+//     let userN = parseInt(prompt('inserisci un numero'));
+//     array.push(userN);
+//     for (let i = 0; i < array.length; i++) {
+//         const thisElement = array[i];
+//         sum += thisElement;
+//     } 
+
+// } while(sum < 50);
 
 
 
-console.log(array,sum);
+// console.log(array,sum);
 
 // while(sum < 50){
 //     const userN = parseInt(prompt('inserisci un numero')); 
