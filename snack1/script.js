59df3746669e07fda7a5a15716fa933e7e513a39
Gabry17 <gbianchi528@gmail.com>
2022-05-04 19:04:07 +0200

    usato funzioni nello snack1

--- snack1/script.js
+++ snack1/script.js
@@ -1,7 +1,7 @@
 //crea 2 array con lunghezze diverse
 //all array piu corto aggiungere elementi mancanti per essere lungo quanto l altro
 const firstArray = [34,67,89,20];
-const secondArray = [55,90];
+const secondArray = [55,90,66,44,33,22,11,22,22,66,55,7];
 
 let diffLength = 0;
 let newArray = [];
@@ -9,16 +9,12 @@ let result;
 
 if(firstArray.length < secondArray.length){
     diffLength = secondArray.length - firstArray.length;
-    for(let i = 0; i < diffLength; i++){
-        newArray.push(Math.floor(Math.random() * 100) + 1);
-    }
+    cicleFor(diffLength,newArray);
     result = firstArray.push(...newArray);
     
 } else if (secondArray.length < firstArray.length){
     diffLength = firstArray.length - secondArray.length;
-    for(let i = 0; i < diffLength; i++){
-        newArray.push(Math.floor(Math.random() * 100) + 1);
-    }
+    cicleFor(diffLength,newArray);
     result = secondArray.push(...newArray);
     
 }
@@ -26,4 +22,21 @@ if(firstArray.length < secondArray.length){
 console.log(firstArray);
 console.log(secondArray);
 
+//FUNCTION
+//funzione che cicla un elemento e pusha nell array numeri casuali
+function cicleFor(element,array) {
+    for(let i = 0; i < element; i++){
+        array.push(getRndInteger(1,100));
+    }
+}
+
+
+
+//funzione che genera un numero casuale
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+//test
+//console.log(getRndInteger(1,5));
 
